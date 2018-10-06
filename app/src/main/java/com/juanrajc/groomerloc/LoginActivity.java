8af4f58b780f9cf80f9af09fc1ecf23bc83e8506
Unknown <juanrajc@hotmail.com>
2018-10-06 17:22:14 +0200

    Pruebas con splash.

--- app/src/main/java/com/juanrajc/groomerloc/LoginActivity.java
+++ app/src/main/java/com/juanrajc/groomerloc/LoginActivity.java
@@ -8,18 +8,11 @@ import android.os.Bundle;
  */
 public class LoginActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-
     }
 
-
-
 }
-
