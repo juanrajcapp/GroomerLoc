744c3c14a3450f49c48557c460eebb80ee447c86
Unknown <juanrajc@hotmail.com>
2018-10-09 22:30:40 +0200

    Activity con API Maps y dependencias Firebase.
    Añado la activity del cliente (tipo Maps) con la API de Google Maps y las dependencias de Firebase.

--- app/src/main/java/com/juanrajc/groomerloc/LoginActivity.java
+++ app/src/main/java/com/juanrajc/groomerloc/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.juanrajc.groomerloc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.*;
 
 /**
  * A login screen that offers login via email/password.
@@ -13,6 +19,26 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login =(Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+    }
+
+    private void login(){
+
+        FirebaseAuth auth;
+
+        EditText email=(EditText) findViewById(R.id.mail);
+        EditText pw=(EditText) findViewById(R.id.pw);
+
+
 
     }
 
