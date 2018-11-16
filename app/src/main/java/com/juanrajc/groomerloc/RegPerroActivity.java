b88fb7ba6514d1e6b1e6a752f4d1fbaeb31d570d
juanrajc APP <juanrajcapp@gmail.com>
2018-11-16 10:16:38 +0100

    Añadido dialog foto perro.
    Añadido un dialog para seleccionar la forma en la que se va a subir una foto del perro que se está registrando.

--- app/src/main/java/com/juanrajc/groomerloc/RegPerroActivity.java
+++ app/src/main/java/com/juanrajc/groomerloc/RegPerroActivity.java
@@ -1,12 +1,17 @@
 package com.juanrajc.groomerloc;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -51,6 +56,52 @@ public class RegPerroActivity extends AppCompatActivity {
 
         //Instancia del botón de registro del perro de la vista.
         botonRegPerro = (Button) findViewById(R.id.botonRegPerro);
+
+    }
+
+    /**
+     * Método que muestra el diálogo que permite seleccionar el método de obtención de imagen.
+     *
+     * @param view
+     */
+    protected void dialogoImagen(View view){
+
+        //Array con las opciones mostradas en el dialog.
+        final CharSequence[] opciones = {"Cámara", "Galería", "Cancelar"};
+
+        //Instancia del dialog con el estilo definido en "styles".
+        AlertDialog.Builder ad = new AlertDialog.Builder(this, R.style.AppTheme_Dialog);
+
+        //Crea un TV para personalizar el título del dialog.
+        TextView tituloDialog = new TextView(this);
+        tituloDialog.setText("¿Qué desea utilizar?");
+        tituloDialog.setGravity(Gravity.CENTER);
+        tituloDialog.setBackgroundColor(Color.GRAY);
+        tituloDialog.setTextColor(Color.WHITE);
+        tituloDialog.setPadding(8, 8, 8, 8);
+        tituloDialog.setTextSize(20);
+
+        //Añade el TV al dialog.
+        ad.setCustomTitle(tituloDialog);
+
+        //Listener que controla cual es la opción seleccionada en el dialog.
+        ad.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if (opciones[i].equals("Cámara")) {
+
+                } else if (opciones[i].equals("Galería")) {
+
+                } else if (opciones[i].equals("Cancelar")) {
+                    dialogInterface.dismiss();
+                }
+
+            }
+        });
+
+        //Muestra el dialog.
+        ad.show();
 
     }
 
