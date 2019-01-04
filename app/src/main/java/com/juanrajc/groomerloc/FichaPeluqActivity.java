709c1d80507f04752731d7162d056b8e5a382c92
juanrajc <juanrajc@hotmail.com>
2019-01-04 21:36:18 +0100

    Creada activity ficha peluquero.

---
+++ app/src/main/java/com/juanrajc/groomerloc/FichaPeluqActivity.java
@@ -0,0 +1,13 @@
+package com.juanrajc.groomerloc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FichaPeluqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ficha_peluq);
+    }
+}
